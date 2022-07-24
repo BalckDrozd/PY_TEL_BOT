471e87f55a1ae57aa8a3a7fb676585c3898f5d01
blackdrozd <Linch007@mail.ru>
2022-07-24 21:32:50 +1000

    Готов версия 1.1

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -1,10 +1,6 @@
-import sqlite3, time
-from create_bot import dp, bot
+import sqlite3
+from create_bot import bot
 from random import randint
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 
 # Пользовательская функция изменения регистра
@@ -30,56 +26,55 @@ async def find_Film_name(message):
     link_img_list = []
     opisanie_list = []
 
-    #Подключаем хромдрайвер
-    ser = Service('/home/blackdrozd/PycharmProjects/pythonTB/chromedriver')
-    options = webdriver.ChromeOptions()
-    options.add_argument("start-maximized")
-    # options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(service=ser, options=options)
-
-    #Поиск названия фильма
+    # Поиск названия фильма
     for name1 in cur.execute('SELECT NAME FROM BD_Film_lordfilm WHERE mylower(NAME) LIKE ?',
                              ('%' + message.text.lower() + '%',)):
+        print(name1)
         name_list.append(name1[0])
+    print(len(name_list))
     for year1 in cur.execute('SELECT YEAR FROM BD_Film_lordfilm WHERE mylower(NAME) LIKE ?',
                              ('%' + message.text.lower() + '%',)):
+        print(year1)
         year_list.append(year1[0])
+    print(len(year_list))
     for kp_rate1 in cur.execute('SELECT KP_RATE FROM BD_Film_lordfilm WHERE mylower(NAME) LIKE ?',
                                 ('%' + message.text.lower() + '%',)):
+        print(kp_rate1)
         kp_rate_list.append(kp_rate1[0])
+    print(len(kp_rate_list))
     for imdb_rate1 in cur.execute('SELECT IMDB_RATE FROM BD_Film_lordfilm WHERE mylower(NAME) LIKE ?',
                                   ('%' + message.text.lower() + '%',)):
+        print(imdb_rate1)
         imdb_rate_list.append(imdb_rate1[0])
+    print(len(imdb_rate_list))
     for link_film1 in cur.execute('SELECT LINK_FILM FROM BD_Film_lordfilm WHERE mylower(NAME) LIKE ?',
                                   ('%' + message.text.lower() + '%',)):
-        # driver.get(link_film1[0])
-        # time.sleep(2)
+        print(link_film1)
         link_film_list.append(link_film1[0])
+    print(len(link_film_list))
     for link_img1 in cur.execute('SELECT LINK_IMG FROM BD_Film_lordfilm WHERE mylower(NAME) LIKE ?',
                                  ('%' + message.text.lower() + '%',)):
+        print(link_img1)
         link_img_list.append(link_img1[0])
+    print(len(link_img_list))
     for opisanie1 in cur.execute('SELECT OPISANIE FROM BD_Film_lordfilm WHERE mylower(NAME) LIKE ?',
                                  ('%' + message.text.lower() + '%',)):
+        print(opisanie1)
+        print(len(opisanie1[0]))
         opisanie_list.append(opisanie1[0])
-
-    # for time_film in link_film_list:
-    #     driver.get(time_film)
-    #     # time.sleep(2)
-    #     # driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
-    #     # element2 = driver.find_element(By.XPATH, """/html/body/div/pjsdiv/pjsdiv[1]/video""")
-    #     print(element2)
-
-
+    print(len(opisanie_list))
 
     print('Поиск закончен')
 
     for i in range(len(name_list)):
-        await bot.send_photo(message.from_user.id, photo=link_img_list[i],
-                             caption=f'Название фильма: {name_list[i]}\nГод: {year_list[i]}\nРейтинг КП: {kp_rate_list[i]}\nРейтинг IMDB: {imdb_rate_list[i]}\nОписание: {opisanie_list[i]}\nСсылка на фильм: {link_film_list[i]}')
+        captions = f'Название фильма: {name_list[i]}\nГод: {year_list[i]}\nРейтинг КП: {kp_rate_list[i]}\nРейтинг IMDB: {imdb_rate_list[i]}\nОписание: {opisanie_list[i]}\nСсылка на фильм: {link_film_list[i]}'
+        print(len(captions))
+        if len(captions) > 1023:
+            captions = f'Название фильма: {name_list[i]}\nГод: {year_list[i]}\nРейтинг КП: {kp_rate_list[i]}\nРейтинг IMDB: {imdb_rate_list[i]}\nОписание: {opisanie_list[i][:-(len(captions) - 1024)]}\nСсылка на фильм: {link_film_list[i]}'
+
+        await bot.send_photo(message.from_user.id, photo=link_img_list[i], caption=captions)
 
     await bot.send_message(message.from_user.id, f'Нашлось {len(name_list)} совпадений')
-
 
 
 async def find_Film_random(message):
@@ -87,7 +82,9 @@ async def find_Film_random(message):
     rnd_film = cur.execute(
         'SELECT NAME, YEAR, KP_RATE, IMDB_RATE, LINK_FILM, LINK_IMG, OPISANIE FROM BD_Film_lordfilm WHERE ID == ?',
         (r,))
-    sfilm=rnd_film.fetchall()[0]
-
+    sfilm = rnd_film.fetchall()[0]
+    capt = f'Название фильма: {sfilm[0]}\nГод: {sfilm[1]}\nРейтинг КП: {sfilm[2]}\nРейтинг IMDB: {sfilm[3]}\nОписание: {sfilm[6]}\nСсылка на фильм: {sfilm[4]}'
+    if len(capt) > 1023:
+        capt = f'Название фильма: {sfilm[0]}\nГод: {sfilm[1]}\nРейтинг КП: {sfilm[2]}\nРейтинг IMDB: {sfilm[3]}\nОписание: {sfilm[6][:-(len(capt) - 1024)]}\nСсылка на фильм: {sfilm[4]}'
     await bot.send_photo(message.from_user.id, photo=sfilm[5],
-                         caption=f'Название фильма: {sfilm[0]}\nГод: {sfilm[1]}\nРейтинг КП: {sfilm[2]}\nРейтинг IMDB: {sfilm[3]}\nОписание: {sfilm[6]}\nСсылка на фильм: {sfilm[4]}')
+                         caption=capt)
